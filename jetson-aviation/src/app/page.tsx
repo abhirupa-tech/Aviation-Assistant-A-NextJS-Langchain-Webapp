3388e5610ae11213d72930c46c9d0c7bc0b4b320
Abhirupa Mitra <mitra.abhirupa@gmail.com>
2024-05-12 09:52:59 +0530

    Adding Parallax changes, and Blog COntainers

--- jetson-aviation/src/app/page.tsx
+++ jetson-aviation/src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 
-import { ParallaxPage } from "@/components/parallaxPage";
+import { FlightCategory } from "@/components/FlightCategory";
+import { ParallaxPage } from "@/components/ParallaxPage";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="" >
       <ParallaxPage/>
+      <FlightCategory/>
     </main>
   );
 }
--- jetson-aviation/src/components/parallaxPage.tsx
+++ jetson-aviation/src/components/parallaxPage.tsx
@@ -1,6 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 import {gsap} from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
+import { TypeAnimation } from "react-type-animation";
 
 
 export const ParallaxPage = () => {
@@ -57,28 +58,28 @@ export const ParallaxPage = () => {
             timeline.to(
                 cloudlayer1.current,
                 {
-                    y: "-=100",
+                    y: "+=4vw",
                 },
                 0
             );
             timeline.to(
                 cloudlayer2.current,
                 {
-                    y: "-=120",
+                    y: "-=0",
                 },
                 0
             );
             timeline.to(
                 cloudlayer3.current,
                 {
-                    y: "-=80",
+                    y: "+=4vw",
                 },
                 0
             );
             timeline.to(
                 cloudlayer4.current,
                 {
-                    y: "-=150",
+                    y: "+=2vw",
                 },
                 0
             );
@@ -88,27 +89,40 @@ export const ParallaxPage = () => {
     },[])
 
 
-    const parallaxCloudBackgroundStyles = 'z-10 absolute bottom-0 w-[100vw]';
-    const parallaxCloudForegroundStyles = 'z-30 absolute bottom-0 w-[100vw]';
+    const parallaxCloudBackgroundStyles = 'z-10 absolute bottom-[10vw] w-[100vw]';
+    const parallaxCloudForegroundStyles = 'z-30 absolute bottom-[10vw] w-[100vw]';    
+    const parallaxCloudTopLayerStyles = 'z-30 absolute bottom-[1vw] w-[100vw]';
     const parallaxStarStyles = 'absolute top-0 w-[100vw]';
-    const parallaxSunStyles = 'z-0 absolute top-[40%] left-[35%] right-[35%] w-[30%]';
+    const parallaxSunStyles = 'z-0 absolute top-[30%] left-[35%] right-[35%] w-[30%]';
     const parallaxFlightStyles = 'z-20 rotate-45 absolute bottom-[-15%] left-[5%] w-[30vw]';
 
     return (
         <div ref={parallaxRef} className="parallax-outer bg-gradient-to-b from-[rgb(24,80,184)] to-[#F4A939] h-[120vh] overflow-hidden overflow-x-none">
             <div className="parallax">
-                <div className ="absolute z-50 drop-shadow-xl top-[50%] w-[100%] leading-tight font-sans text-9xl font-extrabold inline-block text-center align-middle">
+                <div className ="absolute z-50 drop-shadow-xl top-[30%] w-[100%] leading-tight font-sans text-[6vw] font-extrabold inline-block text-center align-middle">
                     <div>Do you want to fly a</div>
-                    <div className="text-yellow-500">Globemaster ?<span className="text-blue-900">|</span></div>
+                    <TypeAnimation className="text-zinc-900"
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'Boeing Globemaster ?',
+                        3000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'Airbus A320 ?',
+                        2500,
+                        'Mig 21 ?',
+                        2000,
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                    />
                 </div>
                 <img ref= {sun} src="./img/Sun.png" className={parallaxSunStyles}/>
                 <img ref= {cloudlayer1} src="./img/Wave L1.png" className = { parallaxCloudBackgroundStyles}/>            
                 <img ref= {flight}src="./img/Designer.png" className = {parallaxFlightStyles}/>
                 <img ref= {cloudlayer2}src="./img/Wave L2.png" className = { parallaxCloudForegroundStyles }/>
                 <img ref= {cloudlayer3} src="./img/Wave L3.png" className = { parallaxCloudForegroundStyles }/>
-                <img ref= {cloudlayer4} src="./img/Wave L4.png" className = { parallaxCloudForegroundStyles }/>
+                <img ref= {cloudlayer4} src="./img/Wave L4.png" className = { parallaxCloudTopLayerStyles }/>
                 <img ref= {starrySky} src="./img/Starry Sky.png" className = {parallaxStarStyles}/>
-                <img ref= {flight} src="./img/Designer.png" className = {parallaxFlightStyles}/>
                 
             </div>
         </div>
